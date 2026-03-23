2ea4a90edc9edc47fbea388889849521fa6e2a58
nirmal tharuka <126665007+Tharukax@users.noreply.github.com>
2026-03-23 09:09:26 +0530

    project init

---
+++ src/main/java/com/healthcare/appointment/AppointmentManagementApplication.java
@@ -0,0 +1,12 @@
+package com.healthcare.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentManagementApplication.class, args);
+    }
+}
